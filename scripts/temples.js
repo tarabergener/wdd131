4d4ebc518587df81913f192a85f6a37b7a2469a4
tarabergener <tara.kelly211@gmail.com>
2024-05-04 01:17:16 -0400

    images

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,1 +1,3 @@
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+document.querySelector('.hamburger').addEventListener('click', function() {
+    document.querySelector('.menu').classList.toggle('show');
+  });
