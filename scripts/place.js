0f43132057e84e667bfd93ac6526c5a95d3e22e6
tarabergener <tara.kelly211@gmail.com>
2024-05-13 23:36:22 -0400

    java changes

--- scripts/place.js
+++ scripts/place.js
@@ -1,1 +1,8 @@
-document.querySelector("#temp").innerHTML = "65";
+const temp = 75
+const wind = 5
+
+
+
+
+document.querySelector("#temp").innerHTML = temp + "&degF";
+document.querySelector("#wind").innerHTML = wind + "km/h";
