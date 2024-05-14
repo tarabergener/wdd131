c76726d3b9f75ea496c256378bbd6254318614c3
tarabergener <tara.kelly211@gmail.com>
2024-05-14 00:34:10 -0400

    script changes

--- scripts/place.js
+++ scripts/place.js
@@ -1,8 +1,10 @@
 const temp = 75
-const wind = 5
+const wind = 7
 
+let velocity = wind * 0.7;
+let windChill = temp - velocity;
 
-
+document.querySelector("#windChill").innerHTML = windChill.toFixed(2) + '&deg;F';
 
 document.querySelector("#temp").innerHTML = temp + "&degF";
 document.querySelector("#wind").innerHTML = wind + "km/h";
